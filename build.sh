e5582437271dd836b02852a853f41c4ad30e0e7b
Oliver <ohardt@gmail.com>
2018-04-08 07:24:43 -0700

    no more email for docker login

--- build.sh
+++ build.sh
@@ -23,10 +23,14 @@ docker version
 gox --osarch="linux/386"   -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter"
 
 echo "Build Docker images"
+
+echo "docker login"
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+echo "docker login done"
+
 docker build --rm=false -t "21zoo/rethinkdb_exporter:$CIRCLE_TAG" .
 docker build --rm=false -t "21zoo/rethinkdb_exporter:latest" .
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 docker push "21zoo/rethinkdb_exporter:latest"
 docker push "21zoo/rethinkdb_exporter:$CIRCLE_TAG"
 
