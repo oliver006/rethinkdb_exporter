14dbfc01c2e6cd5c7296d9ea4105863c65b99cd8
Oliver <ohardt@gmail.com>
2018-04-08 00:13:59 -0700

    fix build

--- build.sh
+++ build.sh
@@ -2,6 +2,22 @@
 
 export CGO_ENABLED=0
 
+echo "Building binaries"
+echo ""
+echo $GO_LDFLAGS
+
+gox -rebuild --osarch="darwin/amd64"  -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter" && cd dist && tar -cvzf rethinkdb_exporter-$CIRCLE_TAG.darwin-amd64.tar.gz rethinkdb_exporter && rm rethinkdb_exporter && cd ..
+gox -rebuild --osarch="darwin/386"    -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter" && cd dist && tar -cvzf rethinkdb_exporter-$CIRCLE_TAG.darwin-386.tar.gz   rethinkdb_exporter && rm rethinkdb_exporter && cd ..
+gox -rebuild --osarch="linux/amd64"   -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter" && cd dist && tar -cvzf rethinkdb_exporter-$CIRCLE_TAG.linux-amd64.tar.gz  rethinkdb_exporter && rm rethinkdb_exporter && cd ..
+gox -rebuild --osarch="linux/386"     -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter" && cd dist && tar -cvzf rethinkdb_exporter-$CIRCLE_TAG.linux-386.tar.gz    rethinkdb_exporter && rm rethinkdb_exporter && cd ..
+gox -rebuild --osarch="netbsd/amd64"  -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter" && cd dist && tar -cvzf rethinkdb_exporter-$CIRCLE_TAG.netbsd-amd64.tar.gz rethinkdb_exporter && rm rethinkdb_exporter && cd ..
+gox -rebuild --osarch="netbsd/386"    -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter" && cd dist && tar -cvzf rethinkdb_exporter-$CIRCLE_TAG.netbsd-386.tar.gz   rethinkdb_exporter && rm rethinkdb_exporter && cd ..
+gox -rebuild --osarch="windows/amd64" -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter" && cd dist && zip -9    rethinkdb_exporter-$CIRCLE_TAG.windows-amd64.zip   rethinkdb_exporter.exe && rm rethinkdb_exporter.exe && cd ..
+gox -rebuild --osarch="windows/386"   -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter" && cd dist && zip -9    rethinkdb_exporter-$CIRCLE_TAG.windows-386.zip     rethinkdb_exporter.exe && rm rethinkdb_exporter.exe && cd ..
+
+echo "Upload to Github"
+ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $CIRCLE_TAG dist/
+
 docker version
 
 gox --osarch="linux/386"   -ldflags "$GO_LDFLAGS" -output "dist/rethinkdb_exporter"
